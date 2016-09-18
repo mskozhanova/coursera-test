16afa3bb03b175ff24c85a4bbdff848b8ca81341
Mariia <ms.kozhanova@gmail.com>
2016-09-18 12:16:12 +0300

    module1

---
+++ module1/app.js
@@ -0,0 +1,38 @@
+(function () {
+'use strict';
+
+angular.module('MsgApp', [])
+.controller('MsgController', MsgController);
+
+MsgController.$inject = ['$scope'];
+function MsgController($scope) {
+  $scope.message = "";
+  $scope.items="";
+  $scope.color="grey";
+
+  $scope.countThem = function () {
+    var lunchItems = $scope.items.split(',');
+    $scope.num = lunchItems.length;
+    if($scope.items=="" )
+      $scope.num=0;
+
+    if($scope.num==0  ) {
+        $scope.message = "Please enter data first";
+        $scope.color="grey";
+      } else {
+        if($scope.num>0){
+          $scope.message = "Enjoy!";
+          $scope.color="green";
+        }
+        if($scope.num>3){
+          $scope.message = "Too much!";
+          $scope.color="red";
+        }
+      }
+  };
+
+
+
+}
+
+})();
